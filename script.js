6d06eff160bce19d2f55d7c0c73c77b49d9745ad
unknown <dzikrialjabar06@gmail.com>
2024-06-05 12:23:15 +0700

    commit all component

---
+++ script.js
@@ -0,0 +1,17 @@
+const toggleBtn = document.getElementById('toggleBtn');
+const menu = document.getElementById('menu');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+    if (menu.classList.contains('hidden')) {
+        // Mengubah ikon menjadi hamburger saat menu disembunyikan
+        toggleBtn.innerHTML = `
+        <i class="fa-solid fa-bars text-lg text-white"></i>
+        `;
+    } else {
+        // Mengubah ikon menjadi silang saat menu ditampilkan
+        toggleBtn.innerHTML = `
+        <i class="fa-solid fa-xmark text-lg text-white"></i>
+        `;
+    }
+});
